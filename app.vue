8c1cb3564065f75a244fb5b93f63c52b50f5abb6
abhinandhan <abhi.devadiga@rabobank.nl>
2025-03-02 11:25:33 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <Html class="bg-bg-primary"/>
+    <NuxtPage/>
+</template>
+
+<script setup lang="ts">
+
+useHead({
+  title: 'Abhinandhan Frontend Developer',
+  meta: [
+    {
+      name: 'description',
+      content: 'Community page'
+    }
+  ],
+  script:[{
+    src: './theme-toggle.js'
+  }]
+})
+
+</script>
---
+++ backlog.md
@@ -0,0 +1,7 @@
+# Wishlist
+
+1. Linkedin Activities integration
+2. Resume download
+3. Joke load every 20seconds - https://www.youtube.com/watch?v=aQPR0xn-MMk&list=PL06MUQt-_wlsRNxmbIvgVuhsXG_dN1XaO&index=4
+
+Home page About Projects Activities
